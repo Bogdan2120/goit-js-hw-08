51a6a94bb2a4eaecdc8521bc6930ee4ff996e64a
Bohdan Vasylyk <bogdanvasilik2120@gmail.com>
2022-11-19 17:41:34 +0200

    Task 8_1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+const getItemTemplate = ({
+  preview,
+  original,
+  description,
+}) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+
+function render() {
+  const item = galleryItems.map(getItemTemplate);
+
+  refs.gallery.insertAdjacentHTML('beforeend', item.join(''));
+}
+
+render();
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
